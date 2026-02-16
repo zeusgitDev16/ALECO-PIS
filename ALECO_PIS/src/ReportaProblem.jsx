765f1f45b670a1f5317f4ee47d9e97cf53efcce1
Emmanuel A. Jacob <emmanuel.jacob@aleco.local>
2026-02-16 12:03:06 +0800

    2/16/2026 -lunch break

--- ALECO_PIS/src/ReportaProblem.jsx
+++ ALECO_PIS/src/ReportaProblem.jsx
@@ -2,42 +2,95 @@ import React from 'react';
 import './CSS/ReportaProblem.css';
 
 const ReportaProblem = () => {
-   return (
-    <div id = "report" className="report-problem-container">
-        <h2 className="report-title">Report a problem</h2>
-        <p className="report-description">
-            Brownouts, damaged posts, broken wires, etc.
-        </p>
+    return (
+        <div id="report" className="report-problem-container">
+            <h2 className="report-title">Report a problem</h2>
+            <p className="report-description">Brownouts, damaged posts, broken wires, etc.</p>
 
-        {/* This container will now stack items vertically */}
-        <div className="report-form-column">
-            {/* Account Number */}
-            <div className="form__group_one">
-                <input type="text" id="acc_num" className="form__group_oneform__field" placeholder="Account Number" />
-                <label htmlFor="acc_num" className="form__group_oneform__label">Account Number</label>
-            </div>
-            {/* First Name */}
-            <div className="form__group_one">
-                <input type="text" id="fname" className="form__group_oneform__field" placeholder="First Name" />
-                <label htmlFor="fname" className="form__group_oneform__label">First Name</label>
-            </div>
-             {/* Middle Name */}
-            <div className="form__group_one">
-                <input type="text" id="mname" className="form__group_oneform__field" placeholder="Middle Name" />
-                <label htmlFor="mname" className="form__group_oneform__label">Middle Name</label>
-            </div>
-             {/* Last Name */}
-            <div className="form__group_one">
-                <input type="text" id="lname" className="form__group_oneform__field" placeholder="Last Name" />
-                <label htmlFor="lname" className="form__group_oneform__label">Last Name</label>
-            </div>
+            <div className="report-main-card">
+                <div className="report-content-wrapper">
+                    
+                    {/* LEFT COLUMN: User Info */}
+                    <div className="report-form-column">
+                        <div className="form__group_one">
+                            <input type="text" id="acc_num" className="form__group_oneform__field" placeholder="Account Number" />
+                            <label htmlFor="acc_num" className="form__group_oneform__label">Account Number</label>
+                        </div>
+                        <div className="form__group_one">
+                            <input type="text" id="fname" className="form__group_oneform__field" placeholder="First Name" />
+                            <label htmlFor="fname" className="form__group_oneform__label">First Name</label>
+                        </div>
+                        <div className="form__group_one">
+                            <input type="text" id="mname" className="form__group_oneform__field" placeholder="Middle Name" />
+                            <label htmlFor="mname" className="form__group_oneform__label">Middle Name</label>
+                        </div>
+                        <div className="form__group_one">
+                            <input type="text" id="lname" className="form__group_oneform__field" placeholder="Last Name" />
+                            <label htmlFor="lname" className="form__group_oneform__label">Last Name</label>
+                        </div>
+                    </div>
 
+                    {/* MIDDLE COLUMN: Concern & Location (Switched) */}
+                    <div className="report-details-column">
+                        <div className="concern-group">
+                            <label htmlFor="concern" className="concern-label">Describe your concern</label>
+                            <textarea 
+                                id="concern" 
+                                className="concern-field textarea-large" 
+                                placeholder="Please explain the problem in detail (e.g., specific pole numbers, wires sparking, etc.)..."
+                            ></textarea>
+                        </div>
+
+                        <div className="concern-group">
+                            <label htmlFor="location" className="concern-label">Address and Location</label>
+                            <input 
+                                type="text" 
+                                id="location" 
+                                className="concern-field" 
+                                placeholder="Enter the complete address" 
+                            />
+                        </div>
+                    </div>
+
+                    {/* RIGHT COLUMN: The Upload Modal (Switched) */}
+                    <div className="report-upload-column">
+                        <div className="modal">
+                            <div className="modal-header">
+                                <div className="modal-logo">
+                                    <span className="logo-circle">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 512 419.116">
+                                            <path d="M16.991,419.116A16.989,16.989,0,0,1,0,402.125V16.991A16.989,16.989,0,0,1,16.991,0H146.124a17,17,0,0,1,10.342,3.513L227.217,57.77H437.805A16.989,16.989,0,0,1,454.8,74.761v53.244h40.213A16.992,16.992,0,0,1,511.6,148.657L454.966,405.222a17,17,0,0,1-16.6,13.332H410.053v.562ZM63.06,384.573H424.722L473.86,161.988H112.2Z" fill="#1cc972" />
+                                        </svg>
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="modal-body">
+                                <p className="modal-title">Upload a picture</p>
+                                <p className="modal-description">pictures about the problem</p>
+                                <button className="upload-area">
+                                    <span className="upload-area-icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 340.531 419.116">
+                                            <path d="M-2904.708-8.885A39.292,39.292,0,0,1-2944-48.177V-388.708A39.292,39.292,0,0,1-2904.708-428h209.558a13.1,13.1,0,0,1,9.3,3.8l78.584,78.584a13.1,13.1,0,0,1,3.8,9.3V-48.177a39.292,39.292,0,0,1-39.292,39.292Zm-13.1-379.823V-48.177a13.1,13.1,0,0,0,13.1,13.1h261.947a13.1,13.1,0,0,0,13.1-13.1V-323.221h-52.39a26.2,26.2,0,0,1-26.194-26.195v-52.39h-196.46A13.1,13.1,0,0,0-2917.805-388.708Zm146.5,241.621a14.269,14.269,0,0,1-7.883-12.758v-19.113h-68.841c-7.869,0-7.87-47.619,0-47.619h68.842v-18.8a14.271,14.271,0,0,1,7.882-12.758,14.239,14.239,0,0,1,14.925,1.354l57.019,42.764c.242.185.328.485.555.671a13.9,13.9,0,0,1,2.751,3.292,14.57,14.57,0,0,1,.984,1.454,14.114,14.114,0,0,1,1.411,5.987,14.006,14.006,0,0,1-1.411,5.973,14.653,14.653,0,0,1-.984,1.468,13.9,13.9,0,0,1-2.751,3.293c-.228.2-.313.485-.555.671l-57.019,42.764a14.26,14.26,0,0,1-8.558,2.847A14.326,14.326,0,0,1-2771.3-147.087Z" transform="translate(2944 428)" fill="#1cc972" />
+                                        </svg>
+                                    </span>
+                                    <span className="upload-area-title">Drag file(s) here to upload.</span>
+                                    <span className="upload-area-description">
+                                        Alternatively, you can select a file by <br /><strong>clicking here</strong>
+                                    </span>
+                                </button>
+                            </div>
+                            <div className="modal-footer">
+                                <button className="btn-secondary">Cancel</button>
+                                <button className="btn-primary">Upload</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div style={{ paddingBottom: '150px' }}></div>
         </div>
-        
-        {/* The big space at the bottom you requested */}
-        <div style={{ paddingBottom: '150px' }}></div>
-    </div>
-);
-}
+    );
+};
 
 export default ReportaProblem;
--- ALECO_PIS/src/components/buttons/login.jsx
+++ ALECO_PIS/src/components/buttons/login.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
-import '../../CSS/Navbar.css';
-
+import React, { useState } from 'react';
+/* Updated path to point to your new file */
+import '../../CSS/LoginContainer.css'; 
 
 const Login = () => {
-    const handleLogin = () => {
-        console.log("Login button clicked!");
-    }
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = () => setShowModal(!showModal);
+
+    const handleBackdropClick = (e) => {
+        if (e.target.className === 'login-modal-overlay') {
+            setShowModal(false);
+        }
+    };
 
     return (
-        <div className = "Login-Container">
-            <button className= "main-login-btn" onClick={handleLogin}>Login</button>
+        <div className="Login-Container">
+            <button className="main-login-btn" onClick={toggleModal}>Login</button>
+
+            {showModal && (
+                <div className="login-modal-overlay" onClick={handleBackdropClick}>
+                    <form className="login-form">
+                        <p id="login-heading">Login</p>
+                        <div className="login-field">
+                            <svg className="login-input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
+                            </svg>
+                            <input autoComplete="off" placeholder="Username" className="login-input-field" type="text" />
+                        </div>
+                        <div className="login-field">
+                            <svg className="login-input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
+                            </svg>
+                            <input placeholder="Password" className="login-input-field" type="password" />
+                        </div>
+                        <div className="login-btn-row">
+                            <button className="login-button1" type="button">Login</button>
+                            <button className="login-button2" type="button">Sign Up</button>
+                        </div>
+                        <button className="login-button3" type="button">Forgot Password</button>
+                    </form>
+                </div>
+            )}
         </div>
     );
-}
+};
 
 export default Login;

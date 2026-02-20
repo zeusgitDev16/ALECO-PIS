b31dc28ba201af074b438d64f88d9d679bfd66d9
Emmanuel A. Jacob <emmanuel.jacob@aleco.local>
2026-02-20 10:14:55 +0800

    feb 20 merging current zeus

--- ALECO_PIS/src/ReportaProblem.jsx
+++ ALECO_PIS/src/ReportaProblem.jsx
@@ -79,7 +79,7 @@ const ReportaProblem = () => {
             <p className="report-description">Brownouts, damaged posts, broken wires, etc.</p>
 
             <div className="report-main-card">
-                <div className="report-content-wrapper">
+                <div className="report-content-wrapper" style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'stretch' }}>
                     
                     {/* LEFT COLUMN: User Info */}
                     <div className="report-form-column">
@@ -128,13 +128,14 @@ const ReportaProblem = () => {
                     </div>
 
                     {/* MIDDLE COLUMN: Concern & Location */}
-                    <div className="report-details-column">
-                        <div className="concern-group">
+                    <div className="report-details-column" style={{ display: 'flex', flexDirection: 'column' }}>
+                        <div className="concern-group" style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
                             <label htmlFor="concern" className="concern-label">Describe your concern</label>
                             <textarea 
                                 id="concern" 
                                 className="concern-field textarea-large" 
                                 placeholder="Please explain the problem in detail (e.g., specific pole numbers, wires sparking, etc.)..."
+                                style={{ flex: 1, resize: 'none' }}
                             ></textarea>
                         </div>
                         <ValidatedInput 
@@ -156,15 +157,15 @@ const ReportaProblem = () => {
                     </div>
 
                     {/* RIGHT COLUMN: The Upload Modal */}
-                    <div className="report-upload-column">
-                        <div className="modal">
+                    <div className="report-upload-column" style={{ display: 'flex', flexDirection: 'column' }}>
+                        <div className="modal" style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
                            <div className="modal-header">
                                 
                             </div>
-                            <div className="modal-body">
+                            <div className="modal-body" style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
                                 <p className="modal-title">Upload a picture</p>
                                 <p className="modal-description">pictures about the problem</p>
-                                <button className="upload-area">
+                                <button className="upload-area" style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: 'auto' }}>
                                     <span className="upload-area-icon">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 340.531 419.116">
                                             <path d="M-2904.708-8.885A39.292,39.292,0,0,1-2944-48.177V-388.708A39.292,39.292,0,0,1-2904.708-428h209.558a13.1,13.1,0,0,1,9.3,3.8l78.584,78.584a13.1,13.1,0,0,1,3.8,9.3V-48.177a39.292,39.292,0,0,1-39.292,39.292Zm-13.1-379.823V-48.177a13.1,13.1,0,0,0,13.1,13.1h261.947a13.1,13.1,0,0,0,13.1-13.1V-323.221h-52.39a26.2,26.2,0,0,1-26.194-26.195v-52.39h-196.46A13.1,13.1,0,0,0-2917.805-388.708Zm146.5,241.621a14.269,14.269,0,0,1-7.883-12.758v-19.113h-68.841c-7.869,0-7.87-47.619,0-47.619h68.842v-18.8a14.271,14.271,0,0,1,7.882-12.758,14.239,14.239,0,0,1,14.925,1.354l57.019,42.764c.242.185.328.485.555.671a13.9,13.9,0,0,1,2.751,3.292,14.57,14.57,0,0,1,.984,1.454,14.114,14.114,0,0,1,1.411,5.987,14.006,14.006,0,0,1-1.411,5.973,14.653,14.653,0,0,1-.984,1.468,13.9,13.9,0,0,1-2.751,3.293c-.228.2-.313.485-.555.671l-57.019,42.764a14.26,14.26,0,0,1-8.558,2.847A14.326,14.326,0,0,1-2771.3-147.087Z" transform="translate(2944 428)" fill="#1cc972" />

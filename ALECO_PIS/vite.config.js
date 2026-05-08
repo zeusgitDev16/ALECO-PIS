c4a2451e98a31325476886f3b6f2e50bed06693a
Zeus <aezymillete16@gmail.com>
2026-05-08 16:29:02 +0800

    may 08 adjustments

--- ALECO_PIS/vite.config.js
+++ ALECO_PIS/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -23,7 +22,7 @@ export default defineConfig(({ mode }) => {
         .replace(/\r?\n\s*<link rel="canonical"[^>]*>\s*\r?\n?/i, '\n')
         .replace(/\r?\n\s*<meta property="og:url"[^>]*>\s*\r?\n?/i, '\n');
     },
-  }, cloudflare()],
+  }],
   server: {
     allowedHosts: [
       'hybridisable-sariah-animatedly.ngrok-free.dev'

605bf08df4358ab870f0846de10bcf1997f4c6b1
Zeus <aezymillete16@gmail.com>
2026-05-11 11:49:21 +0800

    predebut v1

--- ALECO_PIS/backend/config/corsOrigins.js
+++ ALECO_PIS/backend/config/corsOrigins.js
@@ -16,6 +16,7 @@ export function normalizeOrigin(origin) {
  */
 export function buildAllowedCorsOrigins() {
     const defaults = [
+        'https://apisph.org',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:4173',

71dcac8a360335df221b80400f5f986169753e38
Zeus <aezymillete16@gmail.com>
2026-05-16 11:10:12 +0800

    Auto-deploy: Sat, May 16, 2026 11:10:12 AM

--- ALECO_PIS/src/components/modals/ManageSiteModal.jsx
+++ ALECO_PIS/src/components/modals/ManageSiteModal.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import { toast } from 'react-toastify';
 import { apiUrl } from '../../utils/api';
 import { authMutation } from '../../utils/authMutation';
